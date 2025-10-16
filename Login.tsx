5196718acaefe177a43db10dc7fdb359784f6240
dicarpio212 <122206951+dicarpio212@users.noreply.github.com>
2025-10-16 09:03:05 +0700

    Update Login.tsx

--- Login.tsx
+++ Login.tsx
@@ -3,11 +3,11 @@ import React, { useState, useEffect, useRef } from 'react';
 import type { User, UserRole } from '../types';
 import UserIcon from './components/icons/UserIcon';
 import { useTheme } from './ThemeContext';
-import QuestionMarkCircleIcon from './icons/QuestionMarkCircleIcon';
-import LoginInfoModal from './modals/LoginInfoModal';
-import EyeIcon from './icons/EyeIcon';
-import EyeSlashIcon from './icons/EyeSlashIcon';
-import { getAvailableClassTypes } from '../constants';
+import QuestionMarkCircleIcon from './components/icons/QuestionMarkCircleIcon';
+import LoginInfoModal from './components/modals/LoginInfoModal';
+import EyeIcon from './components/icons/EyeIcon';
+import EyeSlashIcon from './components/icons/EyeSlashIcon';
+import { getAvailableClassTypes } from './constants';
 
 
 interface LoginProps {
